5ab5b5f489fa1f3c419a676e8778096e38e71586
RindratianaRatsimandavana <rindratianaratsimandavana@gmail.com>
2024-04-14 17:00:52 +0300

    Écriture des pages JSF et des backing beans associés

---
+++ src/main/java/mg/itu/tpbanqueratsimandavanarindratianaholiniana/jsf/ListeComptes.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package mg.itu.tpbanqueratsimandavanarindratianaholiniana.jsf;
+
+import jakarta.inject.Named;
+import jakarta.faces.view.ViewScoped;
+import jakarta.inject.Inject;
+import java.io.Serializable;
+import java.util.List;
+import mg.itu.tpbanqueratsimandavanarindratianaholiniana.entity.CompteBancaire;
+import mg.itu.tpbanqueratsimandavanarindratianaholiniana.service.GestionnaireCompte;
+
+/**
+ *
+ * @author rindr
+ */
+@Named(value = "listeComptes")
+@ViewScoped
+public class ListeComptes implements Serializable {
+
+    private List<CompteBancaire> allComptes;
+    
+    @Inject
+    private GestionnaireCompte gestionnaireCompte;
+    
+    public ListeComptes() {
+    }
+    
+    
+    /**
+     * Retourne la liste des comptes pour affichage dans une DataTable.
+     */
+    public List<CompteBancaire> getAllComptes() {
+        if (allComptes == null) {
+            allComptes = gestionnaireCompte.getAllCompteBancaires();
+        }
+        return allComptes;
+    }
+    
+}
