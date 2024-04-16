1a0a39bd1d6ed0b69af7ec6e8acf7a0ffd7116e5
RindratianaRatsimandavana <rindratianaratsimandavana@gmail.com>
2024-04-16 04:16:12 +0300

    Déposer ou retirer de l'argent sur un compte

---
+++ src/main/java/mg/itu/tpbanqueratsimandavanarindratianaholiniana/jsf/Mouvement.java
@@ -0,0 +1,105 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package mg.itu.tpbanqueratsimandavanarindratianaholiniana.jsf;
+
+import jakarta.faces.application.FacesMessage;
+import jakarta.faces.component.UIComponent;
+import jakarta.faces.component.UIInput;
+import jakarta.faces.context.FacesContext;
+import jakarta.faces.validator.ValidatorException;
+import jakarta.inject.Named;
+import jakarta.faces.view.ViewScoped;
+import jakarta.inject.Inject;
+import java.io.Serializable;
+import mg.itu.tpbanqueratsimandavanarindratianaholiniana.entity.CompteBancaire;
+import mg.itu.tpbanqueratsimandavanarindratianaholiniana.jsf.util.Util;
+import mg.itu.tpbanqueratsimandavanarindratianaholiniana.service.GestionnaireCompte;
+
+/**
+ * Backing bean pour la page qui effectue des mouvements sur un compte.
+ *
+ * @author rindr
+ */
+@Named(value = "mouvement")
+@ViewScoped
+public class Mouvement implements Serializable {
+
+    private Long id;
+    private CompteBancaire compte;
+    private String typeMouvement;
+    private int montant;
+
+    @Inject
+    private GestionnaireCompte gestionnaireCompte;
+
+    public int getMontant() {
+        return montant;
+    }
+
+    public void setMontant(int montant) {
+        this.montant = montant;
+    }
+
+    public String getTypeMouvement() {
+        return typeMouvement;
+    }
+
+    public void setTypeMouvement(String typeMouvement) {
+        this.typeMouvement = typeMouvement;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public CompteBancaire getCompte() {
+        return compte;
+    }
+
+    public void loadCompte() {
+        compte = gestionnaireCompte.getCompteById(id);
+    }
+
+    /**
+     * Méthode validatrice pour le montant du mouvement. Remarque : La méthode
+     * doit toujours avoir cette signature.
+     *
+     * @param fc
+     * @param composant le composant JSF dans lequel on valide.
+     * @param valeur valeur à valider (le montant pour ce cas)
+     */
+    public void validateSolde(FacesContext fc, UIComponent composant, Object valeur) {
+        UIInput composantTypeMouvement = (UIInput) composant.findComponent("typeMouvement");
+        String valeurTypeMouvement = (String) composantTypeMouvement.getLocalValue();
+        if (valeurTypeMouvement == null) {
+            return;
+        }
+        if (valeurTypeMouvement.equals("retrait")) {
+            int retrait = (int) valeur;
+            if (compte.getSolde() < retrait) {
+                FacesMessage message
+                        = new FacesMessage(FacesMessage.SEVERITY_ERROR,
+                                "Le retrait doit être inférieur au solde du compte",
+                                "Le retrait doit être inférieur au solde du compte");
+                throw new ValidatorException(message);
+            }
+        }
+    }
+
+    public String enregistrerMouvement() {
+        if (typeMouvement.equals("ajout")) {
+            gestionnaireCompte.deposer(compte, montant);
+        } else {
+            gestionnaireCompte.retirer(compte, montant);
+        }
+        Util.addFlashInfoMessage("Mouvement enregistré sur compte de " + compte.getNom());
+        return "listeComptes?faces-redirect=true";
+    }
+
+}
